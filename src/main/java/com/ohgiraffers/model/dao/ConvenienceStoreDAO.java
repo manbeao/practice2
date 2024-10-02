f7a0981dd95beb94c549250c6b907ba3844b6eb8
manbeao <lesles0906@naver.com>
2024-10-02 16:10:19 +0900

    1002

--- src/main/java/com/ohgiraffers/model/dao/ConvenienceStoreDAO.java
+++ src/main/java/com/ohgiraffers/model/dao/ConvenienceStoreDAO.java
@@ -13,6 +13,7 @@ public class ConvenienceStoreDAO {
 
     private Properties prop = new Properties();
 
+    //쿼리 불러오기
     public ConvenienceStoreDAO(){
         try {
             prop.loadFromXML(new FileInputStream("src/main/java/com/ohgiraffers/mapper/ConvenienceStore-query.xml"));
@@ -22,6 +23,7 @@ public class ConvenienceStoreDAO {
 
     }
 
+    //모든 편의점 테이블의 모든 상품 정보 조회
     public void selectAllProductlist (Connection con){
         Statement stmt = null;
         ResultSet rset = null;
@@ -43,6 +45,7 @@ public class ConvenienceStoreDAO {
         }
     }
 
+    // 관리자 ID 확인
     public List<Map<Integer, String>> checkManagerId(Connection con){
         Scanner sc = new Scanner(System.in);
         PreparedStatement pstmt = null;
@@ -93,6 +96,7 @@ public class ConvenienceStoreDAO {
         return checkList;
     }
 
+    //직원 테이블의 모든 직원정보 조회
     public void staffAllInformation(Connection con){
         Statement stmt = null;
         ResultSet rset = null;
@@ -114,6 +118,7 @@ public class ConvenienceStoreDAO {
         }
     }
 
+    //관리자가 관리하는 직원의 정보 조회
     public void myStaffList(Connection con){
         Scanner sc = new Scanner(System.in);
         PreparedStatement ptsmt = null;
@@ -142,6 +147,131 @@ public class ConvenienceStoreDAO {
 
     }
 
+
+    public void StaffManagement(Connection con){
+        Scanner sc = new Scanner(System.in);
+        PreparedStatement pstmt = null;
+        ResultSet rset = null;
+        int result =0;
+
+        System.out.println("1. 직원 추가  2. 직원변경");
+        System.out.println("3. 직원 검색  4. 직원삭제");
+        System.out.println();
+        System.out.print("실행할 메뉴를 입력하세요 : ");
+        int num = sc.nextInt();
+
+        switch (num){
+            case 1:
+                System.out.print("추가할 직원의 이름을 입력 하세요 : ");
+                sc.nextLine();
+                String staffName = sc.nextLine();
+                System.out.print("추가할 직원의 전화번호를 입력하세요 : ");
+                String staffPhone = sc.nextLine();
+                System.out.print("추가할 직원의 입사일을 입력하세요(예-2023.04.08) : ");
+                String staffHireDate = sc.nextLine();
+                System.out.print("추가할 직원의 출근 상태를 입력하세요(O/X) : ");
+                String staffWorkStatus = sc.nextLine();
+                System.out.print("추가할 직원을 관리하는 관리자의 이름을 입력하세요 : ");
+                String managerName = sc.nextLine();
+
+                String query = prop.getProperty("AddStaff");
+                try {
+                    pstmt = con.prepareStatement(query);
+                    pstmt.setString(1,staffName);
+                    pstmt.setString(2,staffPhone);
+                    pstmt.setString(3,staffHireDate);
+                    pstmt.setString(4,staffWorkStatus);
+                    pstmt.setString(5,managerName);
+
+                    result = pstmt.executeUpdate();
+
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                if (result>0){
+                    System.out.println("직원 정보를 성공적으로 추가했습니다");
+                }else {
+                    System.out.println("직원 정보 등록에 실패했습니다");
+                }break;
+
+            case 2:
+                System.out.print("변경할 직원의 직원 ID를 입력 하세요 : ");
+                sc.nextLine();
+                String staffId = sc.nextLine();
+                System.out.print("변경된 직원의 이름을 입력하세요 : ");
+                String staffNameCh = sc.nextLine();
+                System.out.print("변경된 직원의 전화번호를 입력하세요 : ");
+                String staffPhoneCh = sc.nextLine();
+                System.out.print("변경된 직원의 입사일을 입력하세요(예-2023.04.08) : ");
+                String staffHireDateCh=sc.nextLine();
+                System.out.print("변경된 직원의 출근 상태를 입력하세요(O/X) : ");
+                String staffWorkStatusCh = sc.nextLine();
+                System.out.print("변경된 직원을 관리하는 관리자의 이름을 입력하세요 : ");
+                String managerNameCh = sc.nextLine();
+
+                String query1 = prop.getProperty("changeStaff");
+                try {
+                    pstmt = con.prepareStatement(query1);
+
+                    pstmt.setString(1,staffNameCh);
+                    pstmt.setString(2,staffPhoneCh);
+                    pstmt.setString(3,staffHireDateCh);
+                    pstmt.setString(4,staffWorkStatusCh);
+                    pstmt.setString(5,managerNameCh);
+                    pstmt.setString(6,staffId);
+
+                    result = pstmt.executeUpdate();
+
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }if (result>0){
+                System.out.println("직원 정보를 성공적으로 변경했습니다");
+            }else {
+                System.out.println("직원 정보 변경에 실패했습니다");
+            }break;
+
+
+            case 3:
+                System.out.print("검색할 직원의 직원 ID를 입력하세요 : ");
+                int staffIdSh = sc.nextInt();
+
+                String query2 = prop.getProperty("searchStaff");
+                try {
+                    pstmt = con.prepareStatement(query2);
+                    pstmt.setInt(1,staffIdSh);
+
+                    rset = pstmt.executeQuery();
+                    if (rset.next()){
+                        System.out.println("직원 ID : " + rset.getInt("STAFF_ID") + ", 직원 이름 : " + rset.getString("STAFF_NAME") + ", 직원 전화번호 : " + rset.getString("STAFF_PHONE") + ", 입사일 : " + rset.getString("HIRE_DATE") + ", 출근여부 : " + rset.getString("WORK_STATUS") + ", 관리자 이름 : " + rset.getString("MANAGER_NAME"));
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }break;
+
+            case 4:
+                System.out.print("삭제할 직원의 직원 ID를 입력하세요 : ");
+                int staffIdDel = sc.nextInt();
+                String query3 = prop.getProperty("deleteStaff");
+
+                try {
+                    pstmt =con.prepareStatement(query3);
+                    pstmt.setInt(1,staffIdDel);
+
+                    result = pstmt.executeUpdate();
+
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }if (result>0){
+                System.out.println("직원 정보를 성공적으로 삭제했습니다");
+            }else {
+                System.out.println("직원 정보를 삭제하지 못했습니다");
+            }break;
+            default:
+                System.out.println("잘못된 번호를 입력했습니다");
+                break;
+        }
+    }
+    //편의점 테이블에 상품 추가
     public int insertProduct(Connection con) {
 
         Scanner sc = new Scanner(System.in);
@@ -184,31 +314,126 @@ public class ConvenienceStoreDAO {
     }
 
 
+    //상품 조회
     public void searchProduct(Connection con){
         Scanner sc = new Scanner(System.in);
         PreparedStatement pstmt = null;
         ResultSet rset = null;
 
-        String query = prop.getProperty("searchProduct");
 
-        System.out.print("검색할 상품의 상품코드를 입력하세요 : ");
-        int productCode = sc.nextInt();
-        try {
-            pstmt = con.prepareStatement(query);
-            pstmt.setInt(1,productCode);
-            rset = pstmt.executeQuery();
-            if (rset.next()){
-                System.out.println("상품관리 코드 : " + rset.getInt("PRODUCT_CODE") + ", 상품 이름 : " + rset.getString("PRODUCT_NAME") + ", 상품가격 : " + rset.getInt("PRODUCT_PRICE") + ", 상품 카테고리 : " + rset.getString("PRODUCT_CATEGORY") + ", 재고 여부 : " + rset.getString("PRODUCT_STATUS"));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }finally {
-            close(pstmt);
-            close(rset);
+        System.out.println("1. 상품코드로 검색     2. 상품 이름으로 검색    ");
+        System.out.println("3. 상품가격으로 검색   4. 상품 카테고리로 검색 ");
+        System.out.println("5. 상품 재고여부로 검색");
+        System.out.println();
+        System.out.print("상품을 검색할 방법을 입력하세요 : ");
+        int num = sc.nextInt();
+        switch (num){
+            case 1 :
+                //편의점 테이블에서 상품코드로 상품조회
+                String query = prop.getProperty("searchProductCode");
+                System.out.print("검색할 상품의 상품코드를 입력하세요 : ");
+                int productCode = sc.nextInt();
+                try {
+                    pstmt = con.prepareStatement(query);
+                    pstmt.setInt(1,productCode);
+                    rset = pstmt.executeQuery();
+                    if (rset.next()){
+                        System.out.println("상품관리 코드 : " + rset.getInt("PRODUCT_CODE") + ", 상품 이름 : " + rset.getString("PRODUCT_NAME") + ", 상품가격 : " + rset.getInt("PRODUCT_PRICE") + ", 상품 카테고리 : " + rset.getString("PRODUCT_CATEGORY") + ", 재고 여부 : " + rset.getString("PRODUCT_STATUS"));
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("상품코드가 " + productCode + "번인 상품 검색을 완료했습니다");
+                break;
+
+            case 2 :
+                //편의점 테이블에서 상품이름으로 상품조회
+                String query1 = prop.getProperty("searchProductName");
+                System.out.print("검색할 상품의 이름을 입력하세요 : ");
+                sc.nextLine();
+                String productName = sc.nextLine();
+
+                try {
+                    pstmt = con.prepareStatement(query1);
+                    pstmt.setString(1,productName);
+                    rset = pstmt.executeQuery();
+                    while (rset.next()){
+                        System.out.println("상품관리 코드 : " + rset.getInt("PRODUCT_CODE") + ", 상품 이름 : " + rset.getString("PRODUCT_NAME") + ", 상품가격 : " + rset.getInt("PRODUCT_PRICE") + ", 상품 카테고리 : " + rset.getString("PRODUCT_CATEGORY") + ", 재고 여부 : " + rset.getString("PRODUCT_STATUS"));
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("상품이름이 " + productName + "인 상품 검색을 완료했습니다");
+                break;
+
+            case 3:
+                //편의점 테이블에서 상품가격으로 상품조회
+                String query2 = prop.getProperty("searchProductPrice");
+                System.out.print("검색할 상품의 가격을 입력하세요 : ");
+                int productPrice = sc.nextInt();
+
+                try {
+                    pstmt =con.prepareStatement(query2);
+                    pstmt.setInt(1,productPrice);
+                    rset = pstmt.executeQuery();
+                    while (rset.next()){
+                        System.out.println("상품관리 코드 : " + rset.getInt("PRODUCT_CODE") + ", 상품 이름 : " + rset.getString("PRODUCT_NAME") + ", 상품가격 : " + rset.getInt("PRODUCT_PRICE") + ", 상품 카테고리 : " + rset.getString("PRODUCT_CATEGORY") + ", 재고 여부 : " + rset.getString("PRODUCT_STATUS"));
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("가격이 " + productPrice + "인 상품 검색을 완료했습니다");
+                break;
+
+            case 4 :
+                //편의점 테이블에서 상품 카테고리로 상품조회
+                String query3 = prop.getProperty("searchProductCategory");
+                System.out.print("검색할 상품의 카테고리를 입력하세요(식사/음료/주류/위생용품/기타) : ");
+                sc.nextLine();
+                String productCategory = sc.nextLine();
+
+                try {
+                    pstmt = con.prepareStatement(query3);
+                    pstmt.setString(1,productCategory);
+                    rset = pstmt.executeQuery();
+
+                    while (rset.next()){
+                        System.out.println("상품관리 코드 : " + rset.getInt("PRODUCT_CODE") + ", 상품 이름 : " + rset.getString("PRODUCT_NAME") + ", 상품가격 : " + rset.getInt("PRODUCT_PRICE") + ", 상품 카테고리 : " + rset.getString("PRODUCT_CATEGORY") + ", 재고 여부 : " + rset.getString("PRODUCT_STATUS"));
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("상품 카테고리가 "+ productCategory + "인 상품 검색을 완료했습니다");
+                break;
+
+            case 5 :
+                //편의점 테이블에서 상품 재고여부로 상품조회
+                String query4 = prop.getProperty("searchProductStatus");
+                System.out.print("검색할 상품의 재고 여부를 입력하세요(O/X) : ");
+                sc.nextLine();
+                String productStatus = sc.nextLine();
+
+                try {
+                    pstmt = con.prepareStatement(query4);
+                    pstmt.setString(1,productStatus);
+                    rset = pstmt.executeQuery();
+
+                    while (rset.next()){
+                        System.out.println("상품관리 코드 : " + rset.getInt("PRODUCT_CODE") + ", 상품 이름 : " + rset.getString("PRODUCT_NAME") + ", 상품가격 : " + rset.getInt("PRODUCT_PRICE") + ", 상품 카테고리 : " + rset.getString("PRODUCT_CATEGORY") + ", 재고 여부 : " + rset.getString("PRODUCT_STATUS"));
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("상품 재고여부가 " + productStatus + "인 상품 검색을 완료했습니다");
+                break;
+
+            default:
+                System.out.println("번호를 잘못입력하셨습니다");
         }
-        System.out.println(productCode + "번 상품 검색을 완료했습니다");
+
     }
 
+    //편의점 테이블의 상품 변경
     public void changeProduct(Connection con){
         Scanner sc = new Scanner(System.in);
         PreparedStatement ptsmt = null;
@@ -251,6 +476,7 @@ public class ConvenienceStoreDAO {
     }
 
 
+    //편의점 테이블의 상품 삭제
     public void deleteProduct(Connection con){
         PreparedStatement ptsmt = null;
         int result =0;
--- src/main/java/com/ohgiraffers/run/Application.java
+++ src/main/java/com/ohgiraffers/run/Application.java
@@ -29,11 +29,12 @@ public class Application {
         System.out.println("==1. 상품정보 전체 조회");
         System.out.println("==2. 직원정보 전체 조회");
         System.out.println("==3. 내가 관리하는 직원 조회");
-        System.out.println("==4. 상품 추가");
-        System.out.println("==5. 상품 변경");
-        System.out.println("==6. 상품 검색");
-        System.out.println("==7. 상품 삭제");
-        System.out.println("==8. 프로그램 종료");
+        System.out.println("==4. 직원 관리");
+        System.out.println("==5. 상품 추가");
+        System.out.println("==6. 상품 변경");
+        System.out.println("==7. 상품 검색");
+        System.out.println("==8. 상품 삭제");
+        System.out.println("==9. 프로그램 종료");
             System.out.println();
         System.out.print("원하는 메뉴를 선택 하세요 : ");
         int num = sc.nextInt();
@@ -55,22 +56,27 @@ public class Application {
                 break;
 
             case 4:
+                dao.StaffManagement(con);
+                break;
+
+
+            case 5:
                 dao.insertProduct(con);
                 break;
 
-            case 5 :
+            case 6 :
                 dao.changeProduct(con);
                 break;
 
-            case 6 :
+            case 7 :
                 dao.searchProduct(con);
                 break;
 
-            case 7:  dao.deleteProduct(con);
+            case 8:  dao.deleteProduct(con);
                 break;
 
         }
-            if (num==8){
+            if (num==9){
                 System.out.println("프로그램을 종료합니다");
                 break;
             }
