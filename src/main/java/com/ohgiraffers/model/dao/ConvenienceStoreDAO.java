a971ba6537a91d76af2895d9d75376b3e05c0a97
manbeao <lesles0906@naver.com>
2024-10-02 10:53:23 +0900

    1002

--- src/main/java/com/ohgiraffers/model/dao/ConvenienceStoreDAO.java
+++ src/main/java/com/ohgiraffers/model/dao/ConvenienceStoreDAO.java
@@ -40,7 +40,6 @@ public class ConvenienceStoreDAO {
         }finally {
             close(stmt);
             close(rset);
-            close(con);
         }
     }
 
@@ -68,14 +67,17 @@ public class ConvenienceStoreDAO {
                     Map<Integer, String> check = new HashMap<>();
                     check.put(rset.getInt("MANAGER_ID"), rset.getString("MANAGER_NAME"));
                     checkList.add(check);
+                    System.out.println();
                     System.out.println("관리자 코드 확인 완료 :  " + checkList);
 
                     System.out.println("환영합니다 " + managerId + "번 관리자님");
                     System.out.println();
-                }else {
-                    System.out.println("프로그램을 종료합니다");
-
                 }
+
+
+                }else {
+                    System.out.println("관리자 코드가 옳지 않습니다 다시 입력해주세요");
+                    System.out.println();
             }
 
 
@@ -84,7 +86,6 @@ public class ConvenienceStoreDAO {
         }finally {
             close(pstmt);
             close(rset);
-            close(con);
         }
         return checkList;
     }
@@ -99,11 +100,25 @@ public class ConvenienceStoreDAO {
             stmt = con.createStatement();
             rset = stmt.executeQuery(query);
             while (rset.next()){
-                System.out.println("직원 ID : " + rset.getInt("STAFF_ID") + ", 직원 이름 : " + rset.getString("STAFF_NAME") + ", 직원 전화 번호 : " +  rset.getString("STAFF_PHONE") + ", 입사일 : " + rset.getString("HORE_DATE") + ", 출근 여부 : " + rset.getString("WORK_STATUS") + ", 담당 관리자 : ");
+                System.out.println("직원 ID : " + rset.getInt("STAFF_ID") + ", 직원 이름 : " + rset.getString("STAFF_NAME") + ", 직원 전화 번호 : " +  rset.getString("STAFF_PHONE") + ", 입사일 : " + rset.getString("HIRE_DATE") + ", 출근 여부 : " + rset.getString("WORK_STATUS") + ", 담당 관리자 : " + rset.getString("MANAGER_NAME"));
             }
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void myStaffList(Connection con){
+        PreparedStatement ptsmt = null;
+        ResultSet rset = null;
+
+        String query = prop.getProperty("myStaffList");
+//        try {
+//            ptsmt = con.prepareStatement(query);
+//            ptsmt.setInt(1,);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+
     }
 }
--- src/main/java/com/ohgiraffers/run/Application.java
+++ src/main/java/com/ohgiraffers/run/Application.java
@@ -16,12 +16,14 @@ public class Application {
         int result = 0;
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("==== 편의점 관리 프로그램 ====");
-        dao.checkManagerId(con);
+
 
         while (true){
-        System.out.println("==== 프로그램 메뉴 ====");
             System.out.println();
+        System.out.println("==== 편의점 관리 프로그램 ====");
+        dao.checkManagerId(con);
+        System.out.println("==== 프로그램 메뉴 ====");
+        System.out.println();
         System.out.println("==1. 상품정보 전체 조회");
         System.out.println("==2. 직원정보 전체 조회");
         System.out.println("==3. 내가 관리하는 직원 조회");
@@ -29,6 +31,9 @@ public class Application {
         System.out.println("==5. 상품 변경");
         System.out.println("==6. 상품 검색");
         System.out.println("==7. 상품 삭제");
+        System.out.println("==8. 프로그램 종료");
+            System.out.println();
+        System.out.print("원하는 메뉴를 선택 하세요 : ");
         int num = sc.nextInt();
             System.out.println();
 
@@ -39,8 +44,18 @@ public class Application {
                 System.out.println("상품 전체 정보를 출력 완료했습니다");
                 break;
             case 2:
+                dao.staffAllInformation(con);
+                System.out.println();
+                System.out.println("직원 전체 정보를 출력 완료했습니다");
+                break;
+            case 3:
+                dao.myStaffList(con);
         }
-
+            if (num==8){
+                System.out.println("프로그램을 종료합니다");
+                break;
+            }
     }
+
     }
 }
